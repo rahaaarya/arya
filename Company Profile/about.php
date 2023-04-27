23e712a68985e1bad3c030a342f25cec55d0bd38
unknown <raharjaarya666@gmail.com>
2023-04-27 15:38:02 +0700

    naon

---
+++ Company Profile/about.php
@@ -0,0 +1,335 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Logo Title -->
+    <link rel="icon" href="assets/img/logo title.png">
+    <title>STMIK Indonesia</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Font Google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&family=Signika:wght@500&display=swap"
+        rel="stylesheet">
+    <!-- my style -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Icon -->
+    <script src="https://kit.fontawesome.com/cfb1150b77.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent position-fixed w-100">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <nav class="navbar-dark  bg-transparent">
+                    <div class="container-fluid">
+                        <a class="navbar-brand " href="#">
+                            <img src="assets/img/logo title.png" alt="Logo" width="80"
+                                class="d-inline-block align-text-top me-8">STMIK Indonesia
+                        </a>
+                    </div>
+                </nav>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="index.php">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="about.php">ABOUT US</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="product.html" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">PRODUCT &
+                                SERVICE</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="product.php" class="dropdown-item">E-Government &raquo;</a>
+                                    <ul class="dropdown-menu submenu">
+                                        <li><a href="#" class="dropdown-item">E-Performance</a></li>
+                                        <li><a href="#" class="dropdown-item">E-Simpeg</a></li>
+                                        <li><a href="#" class="dropdown-item">E-Nangkis</a></li>
+                                        <li><a href="#" class="dropdown-item">E-Pangan</a></li>
+                                        <li><a href="#" class="dropdown-item">E-Regulasi Tenaga Kesehatan</a></li>
+                                        <li><a href="#" class="dropdown-item">E-Tata Ruang</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="business.php" class="dropdown-item">E-Business &raquo;</a>
+                                    <ul class="dropdown-menu submenu">
+                                        <li><a href="" class="dropdown-item">E-Office</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">CONTACT US</a>
+                        </li>
+                    </ul>
+                    </ul>
+
+
+                </div>
+        </div>
+    </nav>
+    <!-- Hero Section -->
+    <section id="hero">
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-md-6 hero-tagline my-auto">
+                    <h5>About Us</h5>
+                    <h1>We deliver expertise you can trust.</h1>
+                    <p>Apa yang menjadi keunggulan Integra Indonesia adalah solusi yang kami deliver kepada client sudah
+                        proven dan terbukti diimplementasikan di berbagai instansi dan daerah di Indonesia.</p>
+                </div>
+            </div>
+        </div>
+        <img src="assets/img/hero.png" alt="" class="h-100 position-absolute top-0 start-0 ">
+    </section>
+
+    <!-- Overview -->
+    <section id="overview">
+        <div class="container mt-5">
+            <div class="row">
+                <img src="assets/img/img.jpg" alt="" class="image mt-5 w-50 mx-auto">
+            </div>
+            <div class="col-md-6 mx-auto">
+                <h2 class="title-heading-left mt-5">
+                    Overview
+                </h2>
+
+                <p>
+                    STMIK Indonesia sebagai konsultan dan developer dalam bidang
+                    Teknologi Informasi dan Komunikasi (TIK) berusaha memberikan solusi terhadap
+                    permasalahan dan kebutuhan teknologi informasi dengan mengutamakan pada
+                    solusi pemanfaatan IT secara efektif dan efisien untuk mendukung peningkatan
+                    performa dan kinerja.
+                </p>
+            </div>
+    </section>
+
+    <!-- Mission Section -->
+
+    <section id="mission">
+        <div class="container">
+            <di class="row">
+                <div class="col-12 text-center mt-5">
+                    <img src="assets/img/kantor.jpg" alt="" class="float">
+                    <div class="content">
+                        <h2>Our Mission</h2>
+                        <h2>Kami memberikan solusi sistem yang terintegrasi.</h2>
+                        <p>STMIK Indonesia memiliki produk dan layanaan TI di berbagai bidang antara lain impelementasi
+                            di Pemerintahan (E-Government Solution), Perguruan Tinggi dan Sekolah (Academic Solution)
+                            dan dunia bisnis / Industri (Business Solution).</p>
+                        <br>
+                        <p>Apa yang menjadi keunggulan dari STMIK Indonesia adalah solusi yang kami deliver kepada
+                            client sudah proven dan terbukti diimplementasikan di berbagai instansi Pemerintah di
+                            berbagai daerah di Indonesia level Provinsi dan Kabupaten, Perguruan Tinggi Terkemuka dan
+                            Perusahaan.</p>
+                        <br>
+                        <p>Kami selalu melakukan research yang berkelanjutan untuk selalu uptodate dengan perkembangan
+                            teknologi terkini</p>
+                        <br>
+                        <p>Kami memberikan garansi layanan after sales dan pendampingan guna memastikan user
+                            memanfaatkan layanan dan produk sesuai harapan</p>
+                    </div>
+                </div>
+            </di>
+        </div>
+    </section>
+    <br><br><br><br>
+    <section id="values">
+        <div class="container">
+            <div class="row">
+
+                <div class="left">
+                    <h2>Our Values</h2>
+                    <p>STMIK Indonesia mengembangkan <br>
+                        teknologi sesuai dengan Trend yang berkembang <br>
+                        dengan melakukan Continues Reseach and <br>
+                        Development dalam pengembangan produk dan <br>
+                        layanan yang akan kami berikan.</p>
+                </div>
+
+                <div class="right">
+                    <div class="col-md-3 text-center">
+                        <div class="card-icon">
+                            <div class="card-body">
+                                <div class="circle-icon position-relative mx-auto">
+                                    <i
+                                        class="fas fa-user-group fa-2x icon icon position-absolute top=-50 start-50 translate-middle "></i>
+                                </div>
+                                <h5>Solution</h5>
+                                <p>Solusi Pemanfaatan Teknologi Sesuai Kebutuhan</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 text-center">
+                        <div class="card-icon">
+                            <div class="card-body">
+                                <div class="circle-icon position-relative mx-auto">
+                                    <i
+                                        class="fas fa-thumbs-up fa-2x icon icon position-absolute top=-50 start-50 translate-middle"></i>
+                                </div>
+                                <h5>Inovation</h5>
+                                <p>Inovasi Pengembangan Produk sesuai Trend Terkini</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 text-center">
+                        <div class="card-icon">
+                            <div class="card-body">
+                                <div class="circle-icon position-relative mx-auto ">
+                                    <i
+                                        class="fas fa-shield icon fa-2x icon position-absolute top=-50 start-50 translate-middle "></i>
+                                </div>
+                                <h5>Integration</h5>
+                                <p>Solusi Integrasi yang mudah dengan berbagai sistem</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 text-center">
+                        <div class="card-icon">
+                            <div class="card-body">
+                                <div class="circle-icon position-relative mx-auto">
+                                    <i
+                                        class="fas fa-circle-check icon fa-2x icon position-absolute top=-50 start-50 translate-middle "></i>
+                                </div>
+                                <h5>Excellence</h5>
+                                <p>Kami menyiapkan produk yang unggul dan sempurna</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br><br><br><br>
+    <!-- footer -->
+    <section id="footer">
+        <footer class="bg-dark text-white pt -5 pb-4">
+            <div class="container text-center text-md-left">
+                <div class="row text-center text-md-left">
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">
+                            STMIK indonesia
+                        </h5>
+                        <p>STMIK Indonesia sebagai konsultan dan developer dalam bidang Teknologi Informasi dan
+                            Komunikasi (TIK) berusaha memberikan solusi terhadap permasalahan dan kebutuhan teknologi
+                            informasi dengan mengutamakan pada solusi pemanfaatan IT secara efektif dan efisien.</p>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-xl-32 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">
+                            Produk dan Layanan
+                        </h5>
+                        <p>
+                            <a href="#" class="text-white">E-Pangan</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">E-Performance</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">E-Regulasi Nakes</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">Simpeg</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">Simnangkis</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">SIM Tata Ruang</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">E-Office</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">Mobile Apps</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Useful Links</h5>
+                        <p>
+                            <a href="#" class="text-white">Your Account</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">Become Affiliates</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">Shipping Rates</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-white">Help</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact</h5>
+                        <p>
+                            <i class="fas fa-home mr-3"></i>Indonesia, 2023, Bandung
+                        </p>
+                        <p>
+                            <i class="fas fa-envelope mr-3"></i>stmikindonesia@gmail.com
+                        </p>
+                        <p>
+                            <i class="fas fa-phone mr-3"></i>+62 81221918120
+                        </p>
+                        <p>
+                            <i class="fas fa-print mr-3"></i>+01 335 633 77
+                        </p>
+                    </div>
+                </div>
+                <hr class="mb-4">
+                <div class="row align-items-center">
+                    <div class="col-md-7 col-lg-8">
+                        <p>Copyright 042023 All rights reseved by :
+                            <a href="#">
+                                <strong class="text-warning">The Providers</strong>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-md-5 col-lg-4">
+                        <div class="text-center text-md-right">
+                            <ul class="list unstyled list-inline">
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-white"><i
+                                            class="fab fa-facebook"></i></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-white"><i
+                                            class="fab fa-twitter"></i></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-white"><i
+                                            class="fab fa-google-plus"></i></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-white"><i
+                                            class="fab fa-linkedin-in"></i></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn-floating btn-sm text-white"><i
+                                            class="fab fa-youtubes"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </section>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ Company Profile/contact.php
@@ -0,0 +1,167 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Logo Title -->
+    <link rel="icon" href="/assets/img/logo title.png">
+    <title>STMIK Indonesia</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Font Google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&family=Signika:wght@500&display=swap"
+        rel="stylesheet">
+    <!-- my style -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Icon -->
+    <script src="https://kit.fontawesome.com/cfb1150b77.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent position-fixed w-100">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <nav class="navbar-dark  bg-transparent">
+                    <div class="container-fluid">
+                        <a class="navbar-brand " href="#">
+                            <img src="assets/img/logo title.png" alt="Logo" width="80"
+                                class="d-inline-block align-text-top me-8">STMIK Indonesia
+                        </a>
+                    </div>
+                </nav>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="index.php">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">ABOUT US</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="product.php" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">PRODUCT &
+                                SERVICE</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="product.php" class="dropdown-item">E-Government &raquo;</a>
+                                    <ul class="dropdown-menu submenu">
+                                        <li><a href="" class="dropdown-item">E-Performance</a></li>
+                                        <li><a href="" class="dropdown-item">E-Simpeg</a></li>
+                                        <li><a href="" class="dropdown-item">E-Nangkis</a></li>
+                                        <li><a href="" class="dropdown-item">E-Pangan</a></li>
+                                        <li><a href="" class="dropdown-item">E-Regulasi Tenaga Kesehatan</a></li>
+                                        <li><a href="" class="dropdown-item">E-Tata Ruang</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="business.php" class="dropdown-item">E-Business &raquo;</a>
+                                    <ul class="dropdown-menu submenu">
+                                        <li><a href="" class="dropdown-item">E-Office</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="contact.php">CONTACT US</a>
+                        </li>
+                    </ul>
+                    </ul>
+                </div>
+        </div>
+    </nav>
+    <!-- Contact Us -->
+
+    </div>
+    <section id="contact">
+        <div class="contactus">
+            <div class="title">
+                <h1>Contact Us</h1>
+            </div>
+            <div class="box">
+                <div class="contact form">
+                    <h3>Send a Message</h3>
+                    <form action="">
+                        <div class="formBox">
+                            <div class="row50">
+                                <div class="inputBox">
+                                    <span>First Name</span>
+                                    <input type="text" placeholder="Jhon">
+                                </div>
+                                <div class="inputBox">
+                                    <span>Last Name</span>
+                                    <input type="text" placeholder="Doe">
+                                </div>
+                            </div>
+                            <div class="row50">
+                                <div class="inputBox">
+                                    <span>Email</span>
+                                    <input type="text" placeholder="jhondhon@gmail.com">
+                                </div>
+                                <div class="inputBox">
+                                    <span>Mobile</span>
+                                    <input type="text" placeholder="+62 824567890">
+                                </div>
+                            </div>
+                            <div class="row50">
+                                <div class="inputBox">
+                                    <span>Message</span>
+                                    <textarea placeholder="write your message here..."></textarea>
+                                </div>
+                            </div>
+                            <div class="row100">
+                                <div class="inputBox">
+                                    <input type="submit" value="send">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!-- info box -->
+                <div class="contact info">
+                    <h3>Contact Info</h3>
+                    <div class="infoBox">
+                        <div>
+                            <span><i class="fa-solid fa-location-dot"></i></span>
+                            <p>Bandung, Jawa Barat, <br> Indonesia</p>
+                        </div>
+                        <div>
+                            <span><i class="fa-solid fa-envelope"></i></span>
+                            <a href="mailto:stmikindonesia@gmail.com">stmikindonesia@gmail.com</a>
+                        </div>
+                        <div>
+                            <span><i class="fa-solid fa-phone"></i></span>
+                            <a href="tel:+6281221918120">+62 81221918120</a>
+
+                        </div>
+
+                    </div>
+                    <!-- social media links -->
+                    <ul class="sci">
+                        <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fab fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+
+                    </ul>
+
+
+                </div>
+                <!-- map -->
+                <div class="contact map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423286.88275510375!2d-118.7413800593275!3d34.02003924648305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos%20Angeles%2C%20California%2C%20Amerika%20Serikat!5e0!3m2!1sid!2sid!4v1682464742203!5m2!1sid!2sid"
+                        style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+
+    </section>
